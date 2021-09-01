28fbfafdeb8ba487527862d08966e00d0f1bb4db
Kunmun <kunmun.devroms@gmail.com>
2021-09-01 22:03:24 +0530

    tulip: config.fs: Checkout to `LA.UM.10.2.1.r1-03200-sdm660.0`
    • Retain Previous Changes
    
    Change-Id: I8898bb26ca893499070e78dbb4a3b4fff82346b3
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- config.fs
+++ config.fs
@@ -19,6 +19,12 @@ value:2906
 [AID_VENDOR_THERMAL]
 value:2907
 
+[AID_VENDOR_FASTRPC]
+value:2908
+
+[AID_VENDOR_QTR]
+value:2909
+
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
@@ -32,12 +38,6 @@ group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
 
 [vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -85,7 +85,7 @@ user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
-[system/vendor/bin/imsrcsd]
+[vendor/bin/imsdaemon]
 mode: 0755
 user: AID_RADIO
 group: AID_RADIO
